37a639553a360d223ea389442d98023ccdb0cdba
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-07-20 16:50:21 +0200

    style: add Bootstrap

--- files.php
+++ files.php
@@ -5,17 +5,21 @@ $database = new PDO('mysql:host=localhost;dbname=trackbox;charset=utf8', 'root',
 $files = $database->query('SELECT * FROM songs')->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<table>
-    <tr>
-        <th>Name</th>
-        <th>Hash</th>
-        <th>Path</th>
-    </tr>
-    <?php foreach ($files as $file): ?>
+<table class="table table-striped">
+    <thead>
         <tr>
-            <td><?php echo htmlspecialchars($file['name']); ?></td>
-            <td><?php echo htmlspecialchars($file['hash']); ?></td>
-            <td><?php echo htmlspecialchars($file['path']); ?></td>
+            <th>Name</th>
+            <th>Hash</th>
+            <th>Path</th>
         </tr>
-    <?php endforeach; ?>
+    </thead>
+    <tbody>
+        <?php foreach ($files as $file): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($file['name']); ?></td>
+                <td><?php echo htmlspecialchars($file['hash']); ?></td>
+                <td><?php echo htmlspecialchars($file['path']); ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
 </table>
--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TrackBox</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
 <script defer>
     document.addEventListener('DOMContentLoaded', function() {
@@ -39,7 +40,8 @@
     })
 </script>
 <body>
-    <button id="sync-files">Synchroniser</button>
+    <button id="sync-files" class="btn btn-primary">Synchroniser</button>
     <div class="files"></div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
 </html>
