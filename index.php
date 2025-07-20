82be0b8508a10da7535cb20221c55ef5f25bd07e
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-07-20 16:06:44 +0200

    refactor: synchronization with javascript

--- index.php
+++ index.php
@@ -5,9 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<script defer>
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('sync-files').addEventListener('click', function() {
+            fetch ('http://127.0.0.1:5001/sync', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    alert('Files synchronised successfully');
+                } else {
+                    alert('An error happened during synchronization: ' + data.message);
+                }
+            })
+        })
+    })
+</script>
 <body>
-    <form action="http://127.0.0.1:5001/sync" method="POST">
-        <button>Synchroniser</button>
-    </form>
+    <button id="sync-files">Synchroniser</button>
 </body>
 </html>
