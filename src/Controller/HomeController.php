8047b9b89de00cbd75a6b0c24cb982c1ffb72fb7
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-07-21 11:40:51 +0200

    feat: get files list

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,12 @@ class HomeController
         $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../../templates');
         $twig = new \Twig\Environment($loader);
         
-        echo $twig->render('home.html.twig');
+        $database = new \PDO('mysql:host=localhost;dbname=trackbox;charset=utf8', 'root', 'root');
+        $query = "SELECT s1.* FROM songs s1
+                  INNER JOIN (SELECT name, MAX(version) AS max_version FROM songs GROUP BY name) s2
+                  ON s1.name = s2.name AND s1.version = s2.max_version
+                  ORDER BY s1.name";
+        $files = $database->query($query)->fetchAll(\PDO::FETCH_ASSOC);
+        echo $twig->render('home.html.twig', ['files' => $files]);
     }
 }
